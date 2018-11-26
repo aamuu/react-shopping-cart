4e34e9cd90b2c56d160606fc3afa9a4f3587da36
Amit Bidkar <amit.bidkar1@gmail.com>
2018-11-26 18:56:49 +0530

    final commit revised

--- src/data/products.js
+++ src/data/products.js
@@ -8,38 +8,52 @@ const data = [
   },
   {
     id: 2,
-    name: "Laptop 1",
+    name: "Laptop 2",
     price: 30529.0,
     currency: "Rs",
     image: "images/02.jpg"
   },
   {
     id: 4,
-    name: "Laptop 1",
+    name: "Laptop 3",
     price: 40523.0,
     currency: "Rs",
     image: "images/04.jpg"
   },
   {
     id: 6,
-    name: "Laptop 1",
+    name: "Laptop 4",
     price: 11956.0,
     currency: "Rs",
     image: "images/06.jpg"
   },
   {
     id: 5,
-    name: "Laptop 1",
+    name: "Laptop 5",
     price: 59859.0,
     currency: "Rs",
     image: "images/05.jpg"
   },
   {
     id: 3,
-    name: "Laptop 1",
+    name: "Laptop 6",
     price: 145549.0,
     currency: "Rs",
     image: "images/03.jpg"
+  },
+  {
+    id: 7,
+    name: "Laptop 4",
+    price: 21956.0,
+    currency: "Rs",
+    image: "images/06.jpg"
+  },
+  {
+    id: 8,
+    name: "Laptop 5",
+    price: 88859.0,
+    currency: "Rs",
+    image: "images/05.jpg"
   }
 ];
 
