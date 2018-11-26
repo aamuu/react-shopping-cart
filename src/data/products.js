bafca60eb1cc14879d91333cc5726061156f6654
Amit Bidkar <amit.bidkar1@gmail.com>
2018-11-26 18:40:33 +0530

    Initial commit

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: "Laptop 1",
+    price: 80592.0,
+    currency: "Rs",
+    image: "images/01.jpg"
+  },
+  {
+    id: 2,
+    name: "Laptop 1",
+    price: 30529.0,
+    currency: "Rs",
+    image: "images/02.jpg"
+  },
+  {
+    id: 4,
+    name: "Laptop 1",
+    price: 40523.0,
+    currency: "Rs",
+    image: "images/04.jpg"
+  },
+  {
+    id: 6,
+    name: "Laptop 1",
+    price: 11956.0,
+    currency: "Rs",
+    image: "images/06.jpg"
+  },
+  {
+    id: 5,
+    name: "Laptop 1",
+    price: 59859.0,
+    currency: "Rs",
+    image: "images/05.jpg"
+  },
+  {
+    id: 3,
+    name: "Laptop 1",
+    price: 145549.0,
+    currency: "Rs",
+    image: "images/03.jpg"
+  }
+];
+
+export default data;
---
+++ src/items/products.js
@@ -0,0 +1,12 @@
+// reducer
+export default function products(state = []) {
+    return state; 
+}
+
+export function getProducts(state, props) {
+    return state.products;
+}
+
+export function getProduct(state, props) {
+    return state.products.find(item => item.id === props.id);
+}
